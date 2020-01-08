539628b8a67698c73c6d0e2120447ceaa111684d
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-01-08 22:16:41 +0100

    create search data model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(
-	`I imported ${num} from another module called test.js! Variable x is ${x}`
-);
+// API http://forkify-api.herokuapp.com/
+
+// Then, in Recipe.js (as soon as you get there), please replace:
+// const res = await axios(
+// 	`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+// );
+
+import Search from './models/Search';
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+// replace with
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+
+import axios from 'axios';
+
+export default class Search {
+	constructor(query) {
+		this.query = query;
+	}
+
+	async getResults() {
+		try {
+			const res = await axios(
+				`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
+			);
+			this.result = res.data.recipes;
+		} catch (err) {
+			console.log('getResult: ', err);
+		}
+	}
+}
