e97e614dcb5b63a3a1acb33d9f4782ede0c98959
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-01-17 21:39:28 +0100

    create shopping list controller

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -14,6 +15,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
  *  - Linked recipes
  */
 const state = {};
+window.state = state;
 
 // Functions
 
@@ -111,6 +113,40 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
 
+/**
+ * List controller
+ */
+const controlList = () => {
+	// create a new list IF there is non yet
+	if (!state.list) state.list = new List();
+
+	// Add each ingredient to the list and UI
+	state.recipe.ingredients.forEach(el => {
+		const item = state.list.addItem(el.count, el.unit, el.ingredient);
+		listView.renderItem(item);
+	});
+};
+
+// Hnadle delete and update list item events
+
+elements.shopping.addEventListener('click', e => {
+	const id = e.target.closest('.shopping__item').dataset.itemid;
+
+	// delete btn event
+	if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+		// delete from state
+		state.list.deleteItem(id);
+		// delete from UI
+		listView.deleteItem(id);
+	} else if (e.target.matches('.shopping__count-value, .shopping__count-value *')) {
+		const val = parseFloat(e.target.value);
+		state.list.updateCount(id, val);
+	}
+});
+
+/**
+ * Catching btn clicks
+ */
 // handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
 	if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -127,6 +163,7 @@ elements.recipe.addEventListener('click', e => {
 			recipeView.updateServingsIngredients(state.recipe);
 		}
 	}
+	if (e.target.matches('.recipe__btn-add, .recipe__btn-add *')) {
+		controlList();
+	}
 });
-
-window.l = new List();
